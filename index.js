5800bccc8c250e9fed2771fe27aae0f97bd9d9a2
alighamdan <aligame9070@gmail.com>
2021-10-22 00:05:46 +0300

    .

--- index.js
+++ index.js
@@ -16,17 +16,28 @@ button.addEventListener("click", async (event) => {
       return response.json();
     })
     .then(async function (data) {
-      if(!data.list) {
-        return result.value = `No Result Found Try With Another Sentence`
+      if (!data.list) {
+        result.value = `No Result Found Try With Another Sentence`;
       }
       let random = data.list[Math.floor(Math.random() * data.list.length)];
       result.innerHTML = `<b><a href="${
         random.permalink
       }">Definition:</a></b><br />${random.definition}${
-        random.example ? `<br /><br /><b>Example:</b><br /> ${random.example}` : ""
+        random.example
+          ? `<br /><br /><b>Example:</b><br /> ${random.example}`
+          : ""
+      }${
+        (random.sound_urls.length > 0 && random.sound_urls.toString().includes(`api.twilio.com`))
+          ? `<br /> <br /> Audios:${random.sound_urls.filter(e=>e.includes(`api.twilio.com`)).map(e=>{
+            return `<audio src="${e}" controls></audio>`
+          }).join("<br />")}`
+          : ""
       }`;
     })
     .catch((error) => {
       result.value = `Your Country Not Supported!`;
     });
 });
+// random.sound_urls[
+//   random.sound_urls.findIndex((e) => e.includes(`api.twilio.com`))
+// ]
